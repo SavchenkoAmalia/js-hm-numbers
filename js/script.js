a7419e928a5a74900c2c9472a43bd69b7f0710b6
SavchenkoAmalia <ameli.savchenko@gmail.com>
2024-07-13 14:54:55 +0300

    add all

---
+++ js/script.js
@@ -0,0 +1,38 @@
+let celsius = 20;
+let fahrenhei = (celsius * 9) / 5 + 32;
+console.log(fahrenhei);
+
+let dayInMonth = 30;
+let hoursInMonth = dayInMonth * 24;
+let minutesInMonth = hoursInMonth * 60;
+
+console.log(dayInMonth);
+console.log(hoursInMonth);
+console.log(minutesInMonth);
+
+let health = 100;
+let energy = 80;
+console.log(health - 20);
+console.log(energy - 20);
+
+let purchase = 100;
+let discount = 0.1 * purchase;
+let discountedAmount = purchase - discount;
+console.log(discountedAmount);
+
+let num = 7.6;
+console.log(Math.floor(num));
+
+let decimal = "10.5";
+console.log(parseFloat(decimal));
+
+let integer = "10";
+console.log(parseFloat(integer));
+
+let sqrt = 9;
+console.log(Math.sqrt(sqrt));
+
+let value = 10;
+let stringWithNumber = "25";
+console.log(parseInt(stringWithNumber));
+console.log(toString(value));
